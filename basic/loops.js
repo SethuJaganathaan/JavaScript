bf572f75be3fbf90ff333ce49bfec25522b4e20a
SethuJaganathaan <sethuj99@gmail.com>
2024-02-07 11:22:50 +0530

    basic loops

---
+++ basic/loops.js
@@ -0,0 +1,25 @@
+// for(count=1;count<=5;count=count+1){
+//     console.log("Bruhh");
+// }
+
+for(count=1;count<=5;count++){
+    console.log("Bruhh");
+}
+
+for(count=1;count<=5;count++){
+    console.log(count);
+}
+console.log("-")
+for(count=1;count<=5;count=count+2){
+    console.log(count);
+}
+
+console.log("-")
+for(count=5;count>=1;count=count-1){
+    console.log(count);
+}
+
+console.log("-")
+for(count=2;count<=20;count=count+2){
+    console.log(count);
+}
