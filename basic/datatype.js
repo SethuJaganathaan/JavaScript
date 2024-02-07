850f04eafe6b2d104b1fe8c88511641dac235dfb
SethuJaganathaan <sethuj99@gmail.com>
2024-02-07 09:18:30 +0530

    datatypes

---
+++ basic/datatype.js
@@ -0,0 +1,11 @@
+//Primitive 
+console.log(typeof(10));
+console.log(typeof("Alpha"));
+console.log(typeof(1.3));
+console.log(typeof(true));
+var a;
+console.log(typeof(a));
+// null also comes under primitive
+
+
+// Non primitive - object, array
